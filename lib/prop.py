1f8c4343004997e573e908962f2aa31c7a0764ef
David Deng <daviddenghaotian@163.com>
2022-02-04 11:42:18 -0500

    implement SyncedProp using traitlets

--- lib/prop.py
+++ lib/prop.py
@@ -1,11 +1,10 @@
+from traitlets import HasTraits, TraitType, observe
 
-class SyncedProp:
+class SyncedProp(HasTraits):
     """single synced prop to multiple widgets"""
-    # TODO: Inherit from HasTrait? See https://traitlets.readthedocs.io/en/stable/api.html#callbacks-when-trait-attributes-change<2022-02-03, David Deng> #
 
     def __init__(self, *args):
         """ args is a list of 2-tuples (widget, prop) """
-        self._value = None
         self._widget_map = {}  # from id -> widget
         self._synced_props = set()
         self._listeners = []
@@ -13,40 +12,29 @@ class SyncedProp:
             self.sync_prop(widget, prop)
 
     def _observe_cb(self, change):
-        # When the value changes, update the internal model
-        print("change received:")
-        print(change)
+        # print("change received:")
+        # print(change)
         self.value = change['new']
 
     def sync_prop(self, widget, prop):
-        # add new items to map and props, add listeners
-        # assume widget is registered
+        """ Sync a widget's property with the current object.
+
+        :widget: the widget whose property is to be synced
+        :prop: the property name on the widget
+        :returns: None
+
+        """
         wid = id(widget)
         self._widget_map[wid] = widget
         self._synced_props.add((wid, prop))
         widget.observe(self._observe_cb, prop)
         self.value = getattr(widget, prop)
 
-    def observe(self, listener, k):
-        """ compliant with standard widget interface, so that it can be used in interactive_output """
-        if k != 'value':
-            raise KeyError(
-                "Invalid key: {k}. SyncedProp does not have properties other than 'value'")
-        self._listeners.append(listener)
+    value = TraitType()
 
-    @property
-    def value(self):
-        print("getter called")
-        return self._value
-
-    @value.setter
-    def value(self, value):
+    @observe('value')
+    def _observe_value(self, change):
+        value = change['new']
         print(f"setter called with {value}")
         for (wid, prop) in self._synced_props:
             setattr(self._widget_map[wid], prop, value)
-        for listener in self._listeners:
-            change = {'name': 'value', 'old': self._value,
-                      'new': value, 'owner': self, 'type': 'change'}
-            print("emitting change: {}".format(change))
-            listener(change)
-        self._value = value
