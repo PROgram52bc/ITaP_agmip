72f8c496655132151ec5cd4fb92a4040afe8d896
David Deng <daviddenghaotian@163.com>
2022-02-17 13:25:03 -0500

    format R script

--- examples/rfunctions/agmip.fns.r
+++ examples/rfunctions/agmip.fns.r
@@ -15,12 +15,13 @@ read.AgMIP.nc <- function(file, start_year, end_year, yield_crop, var_lon, var_l
     lon <- ncvar_get(ncfile, varid=var_lon)
     lat <- ncvar_get(ncfile, varid=var_lat)
     time <- c(start_year:end_year)
-    
+
     ## Read yields (an array of 720X360X6):
     yield <- ncvar_get(ncfile, varid=yield_crop)
-    
+
+    # Change to three dimensional data
     if(length(dim(yield))==2) {
-      yield <- array(yield, dim=c(dim(yield)[1], dim(yield)[2], 1))
+        yield <- array(yield, dim=c(dim(yield)[1], dim(yield)[2], 1))
     }
     ## Assign the longitudes and latitudes to facilitate merging with
     ## the other files:
@@ -60,36 +61,34 @@ grid.agg <- function(data2agg=NULL, region.map=NULL, agg.function= "production",
     if(agg.function %in% c("summary")){
         agg <- d %>% group_by(id, time) %>%
             summarize(mean = mean(value), median = median(value), sd = sd(value), min = min(value), pctle25 = quantile(value, 0.25), pctle75 = quantile(value, 0.75), max = max(value))
-    }else{
-        if(agg.function %in% c("weighted.m.custom")){
-            if(is.null(weight.map) == TRUE)
-                stop("For using this aggregation option you must upload/specify a weigthing scheme as an argument to weight.map")
-            ## Check weigths file:
-            if(ncol(weight.map)>4)
-                stop('Weights file must have three columns labeled lon, lat, and weight')
-            if((c("lon") %in% names(weight.map) & c("lat") %in% names(weight.map) & c("weight") %in% names(weight.map))==FALSE)
-                stop('Weights file must be labeled lon, lat, and weight')
-            ## If weights file is correct, merge with yields and is data:
-            suppressMessages(d <- left_join(d,weight.map , by =c("lon","lat")))
-            d <- d[complete.cases(d),]
-            ## Weighted Average:
-            agg <- d%>% group_by(id,time) %>% summarize(w.ave.yield = weighted.mean(value, weight))
-        }else{## DEFAULT BEHAVIOR:
-            if(agg.function %in% c("weighted.m")){
-                weight.map <- read.csv( paste(weightMapDir, crop_name, "_hectares_30min.csv", sep = "") )
-                suppressMessages(d <- left_join(d, weight.map, by =c("lon","lat")))
-                d <- d[complete.cases(d),]
-                agg <- d%>% group_by(id,time) %>% summarize(harea.w.yield = weighted.mean(value, weight))
-            }else{## AREA WEIGHTED AVERAGE BEHAVIOR:
-                if(agg.function %in% c("production")){
-                if(is.null(crop_name) == TRUE)stop("For using this aggregation option we need to code an option in the GUI that passess the name (e.g., maize) of the crop.")
-                weight.map <- read.csv( paste(weightMapDir, crop_name, "_hectares_30min.csv", sep = "") )
-                suppressMessages(d <- left_join(d,weight.map, by =c("lon","lat")))
-                d <- d[complete.cases(d),]
-                agg <- d %>% group_by(id,time) %>% summarize(production = sum(value*weight))
-            }else{stop("A proper agg.function must be specified. Acceptable values are: 'production', 'weighted.m', 'summary', and 'weighted.m.custom'")
-              }}}}
-agg
+    }else if(agg.function %in% c("weighted.m.custom")){
+        if(is.null(weight.map) == TRUE)
+            stop("For using this aggregation option you must upload/specify a weigthing scheme as an argument to weight.map")
+        ## Check weigths file:
+        if(ncol(weight.map)>4)
+            stop('Weights file must have three columns labeled lon, lat, and weight')
+        if((c("lon") %in% names(weight.map) & c("lat") %in% names(weight.map) & c("weight") %in% names(weight.map))==FALSE)
+            stop('Weights file must be labeled lon, lat, and weight')
+        ## If weights file is correct, merge with yields and is data:
+        suppressMessages(d <- left_join(d,weight.map , by =c("lon","lat")))
+        d <- d[complete.cases(d),]
+        ## Weighted Average:
+        agg <- d%>% group_by(id,time) %>% summarize(w.ave.yield = weighted.mean(value, weight))
+    }else if(agg.function %in% c("weighted.m")){ ## DEFAULT BEHAVIOR:
+        weight.map <- read.csv( paste(weightMapDir, crop_name, "_hectares_30min.csv", sep = "") )
+        suppressMessages(d <- left_join(d, weight.map, by =c("lon","lat")))
+        d <- d[complete.cases(d),]
+        agg <- d%>% group_by(id,time) %>% summarize(harea.w.yield = weighted.mean(value, weight))
+    }else if(agg.function %in% c("production")){ ## AREA WEIGHTED AVERAGE BEHAVIOR:
+        # if(is.null(crop_name) == TRUE)stop("For using this aggregation option we need to code an option in the GUI that passess the name (e.g., maize) of the crop.")
+        weight.map <- read.csv( paste(weightMapDir, crop_name, "_hectares_30min.csv", sep = "") )
+        suppressMessages(d <- left_join(d,weight.map, by =c("lon","lat")))
+        d <- d[complete.cases(d),]
+        agg <- d %>% group_by(id,time) %>% summarize(production = sum(value*weight))
+    }else{ 
+        stop("A proper agg.function must be specified. Acceptable values are: 'production', 'weighted.m', 'summary', and 'weighted.m.custom'")
+    }
+    agg
 }
 
 
