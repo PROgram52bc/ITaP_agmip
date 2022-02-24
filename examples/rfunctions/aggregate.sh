9ec7a92ce20d9d7d2fdd4b48a329d1f811ca98c1
David Deng <daviddenghaotian@163.com>
2022-02-24 12:00:02 -0500

    added debug code for R script

--- examples/rfunctions/aggregate.sh
+++ examples/rfunctions/aggregate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+file="../../data/raw/PEGASUS/IPSL-CM5A-LR/hist/ssp2/co2/firr/soy/pegasus_ipsl-cm5a-lr_hist_ssp2_co2_firr_yield_soy_annual_1991_2000.nc4" # this one does not work
+file="../../data/epic_hadgem2-es_hist_ssp2_co2_firr_yield_soy_annual_1980_2010.nc4" # this one works
 Rscript \
 	agmip.run.r \
 	agmip.fns.r \
-	../../data/epic_hadgem2-es_hist_ssp2_co2_firr_yield_soy_annual_1980_2010.nc4 \
+	$file \
 	../regionmap/WorldId.csv \
 	pr \
 	"null" \
